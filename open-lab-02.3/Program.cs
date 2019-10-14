1342f610849e5a9ad8cf58a41ea9673bdccdb9bc
martinspilak <martinecko24@gmail.com>
2019-10-14 09:43:59 +0200

    open-lab-02.3

---
+++ open-lab-02.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace open_lab_02._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a % b;
+            Console.WriteLine(c);
+        }
+    }
+}
